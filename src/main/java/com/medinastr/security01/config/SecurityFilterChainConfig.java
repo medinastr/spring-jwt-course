9081eb32484895e758f0176c552b0dd398c00e19
medinastr <pedromstracieri@gmail.com>
2025-07-19 11:52:54 -0300

    chore: add logging properties

--- src/main/java/com/medinastr/security01/config/SecurityFilterChainConfig.java
+++ src/main/java/com/medinastr/security01/config/SecurityFilterChainConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityFilterChainConfig {
@@ -29,16 +29,8 @@ public class SecurityFilterChainConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}user123")
-                .authorities("READ")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$WCg543WFeq1dHzbuxWH4SucStVdpW6ZmyVfqP/QGd1zI.wG01odbK") // Eazy@123
-                .authorities("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
