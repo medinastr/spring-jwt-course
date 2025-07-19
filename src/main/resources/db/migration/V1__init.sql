d93c4cfb9222a920b6d8635dd36e893be9ef024e
medinastr <pedromstracieri@gmail.com>
2025-07-19 01:51:21 -0300

    refactor: migration to create users and authorities

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,14 @@
-CREATE TABLE customer (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(150),
-    password VARCHAR(250),
-    role VARCHAR(100),
-    PRIMARY KEY (id)
+CREATE TABLE users (
+    username VARCHAR(150) NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    enabled BOOLEAN NOT NULL
 );
+
+CREATE TABLE authorities(
+    username VARCHAR(150) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+     CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username
+ON authorities (username, authority);
