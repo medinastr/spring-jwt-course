6d26e9c63d67e5802ce87cf4d46d1753af05ec51
medinastr <pedromstracieri@gmail.com>
2025-07-21 13:47:54 -0300

    feat: create custom user details service class

---
+++ src/main/java/com/medinastr/security01/config/EazyBankUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.medinastr.security01.config;
+
+import com.medinastr.security01.model.entity.Customer;
+import com.medinastr.security01.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPassword(), authorities);
+    }
+}
