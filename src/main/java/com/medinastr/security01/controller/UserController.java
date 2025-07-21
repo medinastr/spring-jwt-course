8e874a37ea801d3946628c6d2dcdb0bded51af56
medinastr <pedromstracieri@gmail.com>
2025-07-21 20:39:16 -0300

    fix: add map struct processor to inject mapper

--- src/main/java/com/medinastr/security01/controller/UserController.java
+++ src/main/java/com/medinastr/security01/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.medinastr.security01.controller;
 
+import com.medinastr.security01.mapper.CustomerMapper;
 import com.medinastr.security01.model.dto.CustomerRegisterDTO;
+import com.medinastr.security01.model.entity.Customer;
+import com.medinastr.security01.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +12,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final CustomerService customerService;
+    private final CustomerMapper customerMapper;
 
     @PostMapping("/customer")
     public ResponseEntity<Void> registerUser(@RequestBody CustomerRegisterDTO customerRegisterDTO) {
+        Customer customer = customerMapper.toEntity(customerRegisterDTO);
+        customerService.createCustomer(customer);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
